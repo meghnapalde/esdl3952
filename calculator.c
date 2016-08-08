e83dacdb2312fd1733d5488477e9a236cd09aa9c
meghnapalde <meghna.palde@cumminscollege.in>
2016-08-08 17:03:29 +0530

    Update calculator.c
    Function subtract is added.

--- calculator.c
+++ calculator.c
@@ -1,12 +1,19 @@
 This is my first file in GitHub.
+
+// This comment is added by using new branch.
 #include <stdio.h>
 void main()
 {
   int a,b,c;
   //Add calculator function here.
  
-  int add(int p,int q)
+  int add (int p,int q)
   {
     return a+b;
+  }
+  
+   int subtract (int p,int q)
+  {
+    return a-b;
   }
 }
